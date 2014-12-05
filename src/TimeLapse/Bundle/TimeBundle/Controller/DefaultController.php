c99b07b6b0aa4112aae4beb0fd301bacf0542526
joris favier <joris.favier@gmail.com>
2014-12-05 23:32:38 +0100

    Ajout de la première route

--- src/TimeLapse/Bundle/TimeBundle/Controller/DefaultController.php
+++ src/TimeLapse/Bundle/TimeBundle/Controller/DefaultController.php
@@ -3,11 +3,18 @@
 namespace TimeLapse\Bundle\TimeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction(Request $request)
     {
-        return $this->render('TimeLapseTimeBundle:Default:index.html.twig', array('name' => $name));
+    	if($request->query->get("q") == "Quelle est ton adresse email ?")
+    		return new Response('joris.favier@gmail.com');
+    	else
+    		return new Response("Salut !!!");
+        //return $this->render('TimeLapseTimeBundle:Default:index.html.twig', array('name' => $name));
     }
 }
