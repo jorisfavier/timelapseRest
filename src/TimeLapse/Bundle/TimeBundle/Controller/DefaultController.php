78aba55c91ed1c75ef758e8db12ed7fd9a72180e
joris favier <joris.favier@gmail.com>
2014-12-06 19:58:31 +0100

    8e route

--- src/TimeLapse/Bundle/TimeBundle/Controller/DefaultController.php
+++ src/TimeLapse/Bundle/TimeBundle/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends Controller
     {
     	if($request->query->get("q") == "Quelle est ton adresse email ?")
     		return new Response('joris.favier@gmail.com');
-    	else if($request->query->get("q") == "Es-tu content de participer ? (OUI/NON)" || $request->query->get("q") == "Es-tu que tu as compris le principe du jeu ? (OUI/NON)" || $request->query->get("q") == "Es-tu pret a recevoir un enonce au format markdown par http post ? (OUI/NON)" || $request->query->get("q") == "As-tu bien recu le premier enonce ? (OUI/NON)")
+    	else if($request->query->get("q") == "Es-tu content de participer ? (OUI/NON)" || $request->query->get("q") == "Es-tu que tu as compris le principe du jeu ? (OUI/NON)" || $request->query->get("q") == "Es-tu pret a recevoir un enonce au format markdown par http post ? (OUI/NON)" || $request->query->get("q") == "As-tu bien recu le premier enonce ? (OUI/NON)" || $request->query->get("q") == "Bravo, je connais tes salles de cours maintenant, es-tu pret pour un nouvel ennonce ? (OUI/NON)")
     		return new Response("OUI");
     	else
     		return new Response("NON");
