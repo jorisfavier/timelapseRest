6be399486dc24cc6d9f44da5e87f08df836b71c1
joris favier <joris.favier@gmail.com>
2014-12-07 02:15:06 +0100

    modif putbisbisbisbisbis

--- src/TimeLapse/Bundle/TimeBundle/Controller/DefaultController.php
+++ src/TimeLapse/Bundle/TimeBundle/Controller/DefaultController.php
@@ -60,12 +60,14 @@ class DefaultController extends Controller
 		$view = FOSView::create();
         $em = $this->getDoctrine()->getManager();
         $repo = $em->getRepository('TimeLapseTimeBundle:Slot');
-        $slot = $repo->findOneBy(array("room" => $id));
+        $res = $repo->findBy(array("room" => $id));
         $data = array();
         $data["description"] = "liste des slots";
         $links = array();
-        $tmp = array('rel'=>"self","uri"=>$this->generateUrl('get_slot', array('id' => $slot->getId()), true));
-        $links[] = array('id'=>$slot->getId(), 'links'=>$tmp);
+        foreach ($res as $slot) {
+            $tmp = array('rel'=>"self","uri"=>$this->generateUrl('get_slot', array('id' => $slot->getId()), true));
+            $links[] = array('id'=>$slot->getId(), 'links'=>$tmp);
+        }
         $data["data"] = $links;
         $view->setStatusCode(200)->setData($data);
         return $view;    	
